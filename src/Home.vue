4675e34e164ccecafe4d3633c50f3a18eeb495b0
vijayendran91 <vijayendran91@gmail.com>
2020-07-14 11:40:57 +0530

    Created basic Vue app

---
+++ src/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>{{msg}}</h1>
+    </div>    
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            msg: "Welcome to the State of Hawaii's public informations page"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
