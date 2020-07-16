c3b1db97539ff8a54a23f66b1830bf32a0f20d5e
vijayendran91 <vijayendran91@gmail.com>
2020-07-16 15:04:46 +0530

    Added Bootstrap and created initial home page.

--- src/Home.vue
+++ src/Home.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="app">
-    <h1>{{msg}}</h1>
+  <div id="app" style="background-color:'#ffffcc';">
+    <title>{{msg}}</title>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="warning">
+        <b-container style="margin-left: 0px;margin-right: 0px;">
+            <b-navbar-brand href="/" style="font-size:30px;">
+                <img src="src/assets/State_of_Hawaii.svg.png"  style="width:10%;">
+                State Of Hawaii
+            </b-navbar-brand>
+            <b-nav-item-dropdown class="navbar-dropdown" toggle-class="text-white" text="Lang" right>
+                <b-dropdown-item href="#">Account</b-dropdown-item>
+                <b-dropdown-item href="#">Settings</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-container>
+      </b-navbar>
+        
+    </div>
+    <div class="home-jumbotron" toggle-class="text-lightyellow">
+        <b-jumbotron header="Unemployment Claims" lead="How to submit a perfect clain ?" style="background-color:'black' !important">
+            <p>This site provides in-detail and step-by-step procedure to submit your unemplayment claim</p>
+            <b-button size="lg" style="background-color:#C7FF0D;">Basic Info</b-button>
+        </b-jumbotron>
+    </div>
     <div id="adobe-dc-view"></div>
+    
   </div>
 </template>
 <script>
@@ -11,32 +33,49 @@ export default {
       msg: "CPIMA"
     };
   },
-  created() {
+//   created() {
     
-    let ckeditor = document.createElement("script");
-    ckeditor.setAttribute("src", "//documentcloud.adobe.com/view-sdk/main.js");
-    document.head.appendChild(ckeditor);
-    document.addEventListener("adobe_dc_view_sdk.ready", function() {
-      var adobeDCView = new AdobeDC.View({
-        clientId: "790af3f947c1460881d99bf4110ab2e2",
-        divId: "adobe-dc-view"
-      });
-      adobeDCView.previewFile(
-        {
-          content: {
-            location: {
-              url:
-                "src/assets/State of Hawaii Guide.pdf"
-            }
-          },
-          metaData: { fileName: "State of Hawaii Guide.pdf" }
-        },
-        {}
-      );
-    });
-  }
+//     let ckeditor = document.createElement("script");
+//     ckeditor.setAttribute("src", "//documentcloud.adobe.com/view-sdk/main.js");
+//     document.head.appendChild(ckeditor);
+//     document.addEventListener("adobe_dc_view_sdk.ready", function() {
+//       var adobeDCView = new AdobeDC.View({
+//         clientId: "790af3f947c1460881d99bf4110ab2e2",
+//         divId: "adobe-dc-view"
+//       });
+//       adobeDCView.previewFile(
+//         {
+//           content: {
+//             location: {
+//               url:
+//                 "src/assets/State of Hawaii Guide.pdf"
+//             }
+//           },
+//           metaData: { fileName: "State of Hawaii Guide.pdf" }
+//         },
+//         {embedMode: "IN_LINE"}
+//       );
+//     });
+//   }
 };
 </script>
 
 <style>
+    .navbar-brand{
+        padding-right: 0px;
+    }
+    body{
+        background-color: lightyellow;
+    }
+
+    .navbar-dropdown{
+        list-style-type: none;
+        color: black;
+    }
+
+    .jumbotron{
+        background-color: lightyellow;
+    }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './Home.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
 new Vue({
   el: '#app',
   render: h => h(App)
